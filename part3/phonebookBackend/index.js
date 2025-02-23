f13dbedb191d780a7a8e51e933d3bcf27140075f
Javier Iregui <iregui.javi@gmail.com>
2025-02-23 17:28:07 +0100

    CORS and Env PORT

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+var corsOptions = {
+	origin: 'http://localhost:5173',
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
@@ -9,27 +16,28 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
+
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{
+		"id": "1",
+		"name": "Arto Hellas",
+		"number": "040-123456"
+	},
+	{
+		"id": "2",
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523"
+	},
+	{
+		"id": "3",
+		"name": "Dan Abramov",
+		"number": "12-43-234345"
+	},
+	{
+		"id": "4",
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122"
+	}
 ]
 
 app.get('/info', (request, response) => {
@@ -91,6 +99,7 @@ app.delete('/api/persons/:id', (request, response) => {
 	response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}...`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}...`)
+})
