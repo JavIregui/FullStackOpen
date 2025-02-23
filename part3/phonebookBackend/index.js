6a014ca21c7e4b0f43fa3baf3adf61c722542f4b
Javier Iregui <iregui.javi@gmail.com>
2025-02-23 18:21:16 +0100

    Update index.js

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -16,8 +16,6 @@ app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-
 let persons = [
 	{
 		"id": "1",
