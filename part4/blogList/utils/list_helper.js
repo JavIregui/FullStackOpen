c6181a09c46571f8bb6012ce6bc2baba1df0b652
Javier Iregui <iregui.javi@gmail.com>
2025-02-24 18:07:51 +0100

    4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,12 +1,15 @@
-const dummy = (blogs) => {
-    return 1
-}
-
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, item) => sum + item.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+    const blog = blogs.find(blog => blog.likes === maxLikes)
+
+    return blog === undefined ? null : blog
+}
+
 module.exports = {
-    dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
