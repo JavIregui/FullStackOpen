40f952c7a9d0c0760e99127dd7a2aa1e121f55a0
Javier Iregui <iregui.javi@gmail.com>
2025-02-27 03:29:47 +0100

    5.3

--- part5/bloglistFront/src/App.jsx
+++ part5/bloglistFront/src/App.jsx
@@ -11,6 +11,10 @@ const App = () => {
 
 	const [user, setUser] = useState(null)
 
+	const [newTitle, setNewTitle] = useState('')
+	const [newAuthor, setNewAuthor] = useState('')
+	const [newUrl, setNewUrl] = useState('')
+
 	useEffect(() => {
 		blogService.getAll().then(blogs =>
 			setBlogs(blogs)
@@ -50,6 +54,22 @@ const App = () => {
 		blogService.setToken(null)
 	}
 
+	const handleNewBlog = async (event) => {
+		event.preventDefault()
+		
+		const blogObject = {
+			title: newTitle,
+			author: newAuthor,
+			url: newUrl,
+		}
+
+		const returnedBlog = await blogService.create(blogObject)
+		setBlogs(blogs.concat(returnedBlog))
+		setNewTitle('')
+		setNewAuthor('')
+		setNewUrl('')
+	}
+
 	const loginForm = () => (
 		<div>
 			<h2>log in to application</h2>
@@ -81,6 +101,38 @@ const App = () => {
 		<div>
 			<h2>blogs</h2>
 			<p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+
+			<form onSubmit={handleNewBlog}>
+				<div>
+					title
+					<input
+						type="text"
+						value={newTitle}
+						name="Title"
+						onChange={({ target }) => setNewTitle(target.value)}
+					/>
+				</div>
+				<div>
+					author
+					<input
+						type="text"
+						value={newAuthor}
+						name="Author"
+						onChange={({ target }) => setNewAuthor(target.value)}
+					/>
+				</div>
+				<div>
+					url
+					<input
+						type="text"
+						value={newUrl}
+						name="Url"
+						onChange={({ target }) => setNewUrl(target.value)}
+					/>
+				</div>
+				<button type="submit">create</button>
+			</form>
+
 			{blogs.map(blog =>
 				<Blog key={blog.id} blog={blog} />
 			)}
