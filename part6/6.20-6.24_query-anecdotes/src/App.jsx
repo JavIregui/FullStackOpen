ec5b1009878a404fe449721578d8f3245878b590
Javier Iregui <iregui.javi@gmail.com>
2025-03-19 00:14:47 +0100

    6.20

--- part6/6.20-6.24_query-anecdotes/src/App.jsx
+++ part6/6.20-6.24_query-anecdotes/src/App.jsx
@@ -1,19 +1,31 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
 
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 2,
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>
